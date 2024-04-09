344741d59caf606f09dae36352154621d696a80e
amygraceandrews <158005080+amygraceandrews@users.noreply.github.com>
2024-04-09 22:55:34 +0100

    jQuery rework

--- scripts.js
+++ scripts.js
@@ -1,11 +1,8 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
  
-
-    $(".hamburger-button").click(function(){
-
+    $('.hamburger-button').click(function(){
+        $('.mobile-menu').fadeToggle(100);
         $(this).toggleClass('active');
-        $(".mobile-menu").fadeToggle();
-    });
-
+      });
 });
