0bd47849540074caf65ec9d2bf5edfcbba16cf0b
amygraceandrews <158005080+amygraceandrews@users.noreply.github.com>
2024-04-10 15:30:23 +0100

    light gallery

--- scripts.js
+++ scripts.js
@@ -1,6 +1,29 @@
+// A $( document ).ready() block.
+
+// Hamburger Menu
+
 $(document).ready(function() {
   $(".hamburger-button").click(function() {
       $(this).toggleClass("active");
       $(".mobile-menu").fadeToggle()
   });
+});
+
+
+
+// Light Gallery
+
+lightGallery(document.getElementById('lightgallery'), {
+  speed: 500,
+  download: false
+  
+});
+
+
+// Light Gallery 2
+
+lightGallery(document.getElementById('lightgallery-2'), {
+  speed: 500,
+  download: false
+  
 });
