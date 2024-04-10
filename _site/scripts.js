e2ece40fdd7655ca68fd55f8af0e57769d182db4
amygraceandrews <158005080+amygraceandrews@users.noreply.github.com>
2024-04-10 11:03:49 +0100

    header nav update

--- _site/scripts.js
+++ _site/scripts.js
@@ -1,8 +1,6 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
- 
-    $('.hamburger-button').click(function(){
-        $('.mobile-menu').fadeToggle(100);
-        $(this).toggleClass('active');
-      });
+$(document).ready(function() {
+  $(".hamburger-button").click(function() {
+      $(this).toggleClass("active");
+      $(".mobile-menu").fadeToggle()
+  });
 });
